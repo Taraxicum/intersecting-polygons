abac62340bfc37796ccb5cff757d49241fbd0f4c
Jonathan Goff <olpabas@gmail.com>
2021-08-21 12:57:46 -0700

    Remove ordering filter since it is enforced in generator, replace old parity filter with Jason's new parity filter

--- test_filters.py
+++ test_filters.py
@@ -1,17 +1,5 @@
-from filters import OrderingFilter, RepeatedStepFilter, MaxFullLengthStepFilter, HappyPathFilter, Lemma1Filter, ParityFilter,SimpleParityFilter
-
-def test_ordering_filter():
-    print("testing ordering filter")
-    f = OrderingFilter(5)
-    assert not f.apply_filter([])
-    assert not f.apply_filter([[]])
-    assert not f.apply_filter([[1, 2, 3, 4]])
-    assert f.apply_filter([[1, 2, 3, 4], [1, 2, 4, 3]])
-    assert not f.apply_filter([[1, 2, 3, 4], [1, 2, 5, 3]])
-    assert f.apply_filter([[1, 2, 3, 4], [1, 3, 5, 2]])
-    assert f.apply_filter([[2, 3, 4], [1, 3, 5, 2]])
-    assert f.apply_filter([[2, 3], [1, 3, 5, 2]])
-    assert not f.apply_filter([[1, 3, 4], [1, 3, 5, 2]])
+from filters import (RepeatedStepFilter, MaxFullLengthStepFilter,
+                    HappyPathFilter, Lemma1Filter, ParityFilter)
 
 def test_repeated_step_filter():
     print("testing repeated step filter")
@@ -71,29 +59,9 @@ def test_parity_filter():
     assert f.apply_filter([[1, 3, 2, 5, 6, 7]])
     assert f.apply_filter([[1, 2, 3, 5, 7, 6]])
     assert not f.apply_filter([[8, 9, 1]]), "should allow valid parity wrt wrapping"
-    assert f.apply_filter([[8, 9, 2, 1]]), "should filter invalid parity wrt wrapping" # not currently implemented so this test is failing
+    assert f.apply_filter([[8, 9, 2, 1]]), "should filter invalid parity wrt wrapping"
 
-def test_simple_parity_filter():
-    print("testing simple parity filter")
-    f = SimpleParityFilter(9)
-    assert not f.apply_filter([])
-    assert not f.apply_filter([[]])
-    assert not f.apply_filter([[1, 2, 3, 4]])
-    assert f.apply_filter([[1, 3, 2, 4]])
-    assert not f.apply_filter([[1, 2, 3, 5, 6]])
-    assert f.apply_filter([[1, 3, 2, 5, 6, 7]])
-    assert f.apply_filter([[1, 2, 3, 5, 7, 6]])
-    assert not f.apply_filter([[8, 9, 1]]), "should allow valid parity wrt wrapping"
-    assert f.apply_filter([[8, 9, 2, 1]]), "should filter invalid parity wrt wrapping" # not currently implemented so this test is failing
-
-
-
-
-[[1, 2, 3, 4], [5, 2, 3, 4], [2, 3, 4], [2, 1, 5, 4], [3, 2, 1, 5], [4, 3, 2, 1]]
-
-#test_parity_filter()
-test_simple_parity_filter()
-test_ordering_filter()
+test_parity_filter()
 test_repeated_step_filter()
 test_max_full_length_step_filter()
 test_happy_path_filter()
