339b43003c6a0e689716e8d2d8a999b3f6c63b9c
Jonathan Goff <olpabas@gmail.com>
2021-08-22 12:53:17 -0700

    adjust ongoing reporting a little

--- flow.py
+++ flow.py
@@ -1,5 +1,6 @@
 from filters import *
 from polygon_intersection_generator import *
+import time
 
 class IntersectingPolygons:
     steps_count = 0
@@ -8,6 +9,8 @@ class IntersectingPolygons:
         self.filter_manager = filter_manager
         #self.generator = PolygonIntersectionGenerator(self.n)
         self.generator = PolygonIntersectionGenerator(self.n)
+        self.start_time = time.time()
+    
     def end_condition(self, candidate_sequence):
         """
         We are looking for a sequence that has final step the reverse of the initial step
@@ -23,8 +26,10 @@ class IntersectingPolygons:
 
     def recursion(self, candidate_sequence):
         self.steps_count += 1
-        #if self.steps_count %1000 == 0: print(f"Sequence count {self.steps_count}, sequence {candidate_sequence}", flush=True)
-        if self.steps_count > 5000: return
+        if self.steps_count %500000 == 0: 
+            elapsed_time = time.time() - self.start_time
+            print(f"Sequence count {self.steps_count}, time {elapsed_time:.1f}, sequence {candidate_sequence}", flush=True)
+        #if self.steps_count > 5000: return
         new_candidate_sequence = self.generator.generate_candidate_step(candidate_sequence)
         for x in new_candidate_sequence:
             if not self.filter_manager.filter_candidate_step(x):
